c3117dce41c52ac5cc9f83817123d83b6c550538
rjktcby <mkolesin@gmail.com>
2016-07-20 12:12:47 +0300

    moved all headers in src/, removed face-detect.mm

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd $script_dir
 mkdir -p build
 cd build
 
-echo "clang++-3.5 -ObjC++ ../src/*.mm -I../include `gnustep-config --objc-flags` -lgnustep-base -lobjc `pkg-config opencv --libs` -o face-detect"
-clang++-3.5 -ObjC++ ../src/*.mm -I../include -Wall `gnustep-config --objc-flags` -lgnustep-base -lobjc `pkg-config opencv --libs` -o face-detect
+echo "clang++-3.5 -ObjC++ ../src/*.mm -I../src `gnustep-config --objc-flags` -lgnustep-base -lobjc `pkg-config opencv --libs` -o face-detect"
+clang++-3.5 -ObjC++ ../src/*.mm -I../src -Wall `gnustep-config --objc-flags` -lgnustep-base -lobjc `pkg-config opencv --libs` -o face-detect
 
 cd $current_dir
